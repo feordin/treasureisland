86049899aa95e01753b3af26889896953a318d76
feordin <jared.erwin@gmail.com>
2014-09-19 16:35:32 -0700

    Test with express

---
+++ app.js
@@ -0,0 +1,10 @@
+﻿var express = require("express");
+var app = express();
+
+app.get("/game.html", function (req, res) {
+    res.send("<HTML><BODY><H1>Treasure Island Welcomes you!</H1></BODY></HTML>");
+});
+
+var server = app.listen(3000, function() {
+    console.log("Listening on port %d", server.address().port);
+});
